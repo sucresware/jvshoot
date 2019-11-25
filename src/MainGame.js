8356e65ca65244ccadef85c42cb0a817aaa26d52
Simon MGK <contact@mgkprod.fr>
2019-11-25 21:46:32 +0100

    Settings rework + storage

--- src/MainGame.js
+++ src/MainGame.js
@@ -8,13 +8,25 @@ import SettingsScene from './scenes/Settings'
 
 import config from './config'
 
-let gameConfig = Object.assign(config, {
+let gameConfig = {
+  ...config,
   scene: [BootScene, SplashScene, MenuScene, GameScene, SettingsScene]
-})
+}
+
+let defaultSettings = {
+  volMusic: 0.5,
+  volEffect: 0.5,
+  particles: true,
+}
+
+window.settings = {
+  ...defaultSettings,
+  ...JSON.parse(localStorage.getItem('settings')),
+};
 
 export default class MainGame extends Phaser.Game {
   constructor (appendConfig) {
-    if (appendConfig) gameConfig = {...gameConfig, ...appendConfig }
+    if (appendConfig) gameConfig = { ...gameConfig, ...appendConfig }
     super(gameConfig)
   }
 }
--- src/scenes/Settings.js
+++ src/scenes/Settings.js
@@ -11,21 +11,12 @@ var selected = 0; // Selected setting (0:VolEffect, 1:volMusic, 2:particlesCB, 3
 
 var particles = true;
 
-
 export default class extends Phaser.Scene {
   constructor () {
     super({ key: 'SettingsScene' })
   }
 
-  preload () {}
-
-  create () {
-    if(localStorage.getItem('volEffect')) { // check if volEffect already exist in browser
-      volEffect = localStorage.getItem('volEffect') // set volEffect to the volEffect storage's value
-    } else {
-      volEffect = 5 // set volEffect to 5 for default
-      localStorage.setItem('volEffect', volEffect) // create a volEffect local storage var
-    }
+  preload () {
     this.bgm = this.sound.add("the_scene_is_dead");
 
     this.bgm.addMarker({
@@ -33,10 +24,12 @@ export default class extends Phaser.Scene {
       start: 4.3,
       duration: 46.5
     });
+  }
 
-    this.bgm.play('intro', {volume: volEffect / 10})
-
+  create () {
+    this.bgm.play('intro', { volume: window.settings.volMusic })
     this.sound.add("explode").play()
+
     this.cameras.main.shake(200, 0.01)
     this.cameras.main.setZoom(2);
     this.cameras.main.zoomTo(1, 50);
@@ -49,34 +42,22 @@ export default class extends Phaser.Scene {
     let title = this.add.bitmapText(this.game.config.width / 2, top, 'white_shadow', 'SETTINGS', 16).setOrigin(0.5)
     this.tweens.add({targets: title, y: top - 10, duration: 2000, ease: 'Sine.easeInOut', repeat: -1, yoyo: true});
 
-
     top += 40;
     this.test = this.add.bitmapText(left, top, 'indigo_shadow', 'SOUNDS', 16)
     top += 28;
 
     this.effectText = this.add.bitmapText(left + 10, top, 'white_shadow', 'EFFECT', 8)
-    this.volEffectText = this.add.bitmapText(this.game.config.width / 2, top, 'white_shadow', volEffect, 8)
-    
-    this.selectedEffect = this.tweens.add({targets: this.effectText, alpha: 0, duration: 0, ease: 'Sine.easeInOut', repeat: -1, repeatDelay: 500, hold: 500, yoyo: true});
-    
+    this.volEffectText = this.add.bitmapText(this.game.config.width / 2, top, 'white_shadow', '', 8)
+
 		this.lSelect = this.add.sprite(this.game.config.width / 2 - 10, top + 4, 'arrow').setScale(-1)
 		this.rSelect = this.add.sprite(this.game.config.width / 2 + 17, top + 4, 'arrow')
 
-    if(volEffect >= 10) {
-      this.rSelect.setVisible(false)
-      this.lSelect.setVisible(true)
-    } else if (volEffect <= 0) {
-      this.bgm.pause()
-      this.rSelect.setVisible(true)
-      this.lSelect.setVisible(false)
-    }
-
     top += 20;
- 
+
     this.musicText = this.add.bitmapText(left + 10, top, 'white_shadow', 'MUSIC', 8)
-    this.add.bitmapText(this.game.config.width / 2, top, 'white_shadow', volMusic, 8)
-    this.add.sprite(this.game.config.width / 2 - 10, top + 4, 'arrow').setScale(-1)
-    this.add.sprite(this.game.config.width / 2 + 17, top + 4, 'arrow')
+    this.volMusicText = this.add.bitmapText(this.game.config.width / 2, top, 'white_shadow', '', 8)
+    this.lSelect2 = this.add.sprite(this.game.config.width / 2 - 10, top + 4, 'arrow').setScale(-1)
+    this.rSelect2 = this.add.sprite(this.game.config.width / 2 + 17, top + 4, 'arrow')
 
     top += 30;
     this.add.bitmapText(left, top, 'indigo_shadow', 'TWEAKS', 16)
@@ -84,35 +65,17 @@ export default class extends Phaser.Scene {
 
     this.particlesText = this.add.bitmapText(left + 10, top, 'white_shadow', 'PARTICLES', 8)
     this.particlesCB = this.add.sprite(this.game.config.width / 2 + 4, top + 4, 'checkbox')
-    this.particlesCBOk = this.add.sprite(this.game.config.width / 2 + 4, top + 4, 'checkbox_ok')
-
-    if (localStorage.getItem('particles')) { // check if particles already exist in browser
-      particles = JSON.parse(localStorage.getItem('particles')) // set particles to the particles storage's value
-    } else {
-      localStorage.setItem('particles', particles) // create a particles local storage var
-    }
-
-    if (particles) {
-      this.particlesCBOk.setVisible(true)
-      this.particlesCB.setVisible(false)
-    } else {
-      this.particlesCBOk.setVisible(false)
-      this.particlesCB.setVisible(true)
-    }
 
     top += 20;
-    this.oncheText = this.add.bitmapText(left + 10, top, 'white_shadow', 'ONCHECUCK', 8)
-
 
     this.add.bitmapText(this.game.config.width / 2, this.game.config.height - 10, 'white_shadow', 'PRESS A FOR MENU', 8).setOrigin(0.5)
 
-
     this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
     this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q)
-    this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z) 
-    this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S) 
-    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE) 
-    this.keyMenu = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A) 
+    this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z)
+    this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+    this.keyMenu = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
 
     watch(this.keyRight, "isDown", (key, action, value) => value ? this.change('right') : '') // Watch if D key is pressed
     watch(this.keyLeft, "isDown", (key, action, value) => value ? this.change('left') : '') // Watch if Q key is pressed
@@ -120,70 +83,60 @@ export default class extends Phaser.Scene {
     watch(this.keyUp, "isDown", (key, action, value) => value ? this.select('up') : '') // Watch if Z key is pressed
     watch(this.keyDown, "isDown", (key, action, value) => value ? this.select('down') : '') // Watch if S key is pressed
     watch(this.keyMenu, "isDown", (key, action, value) => value ? this.goToMenu() : '') // Watch if A key is pressed
-  }
 
-  update () {
+    this.selectedText = [this.effectText, this.musicText, this.particlesText];
+
+    this.change(0)
+    this.refreshGui()
   }
 
   change (key) {
-    if (key === 'right') {
-      switch (selected) {
-        case 0:
-          if(volEffect < 10) {
-            if (volEffect == 0) {this.bgm.resume()} 
-            volEffect++; 
-            this.lSelect.setVisible(true)
-            if (volEffect == 10) {this.volEffectText.x = this.game.config.width / 2 - 5; this.rSelect.setVisible(false);}
-          }    
-          this.volEffectText.text = volEffect
-          localStorage.setItem('volEffect', volEffect)
-          this.bgm.setVolume(volEffect / 10)
+    switch (selected) {
+      case 0:
+        window.settings.volEffect *= 10; // Javascript rulez
+
+        if (key === 'right') window.settings.volEffect++;
+        else if (key === 'left') window.settings.volEffect--;
+
+        window.settings.volEffect = Phaser.Math.Clamp(window.settings.volEffect, 0, 10);
+
+        window.settings.volEffect /= 10;
         break;
-      }
-    } else if (key === 'left') {
-      switch (selected) {
-        case 0:
-          if (volEffect > 0) {
-            volEffect--; 
-            this.volEffectText.x = this.game.config.width / 2;
-            this.rSelect.setVisible(true)
-          }
-          if (volEffect == 0) {
-            this.bgm.pause()
-            this.lSelect.setVisible(false)
-          }
-          this.volEffectText.text = volEffect
-          localStorage.setItem('volEffect', volEffect)
-          this.bgm.setVolume(volEffect / 10)
+      case 1:
+          window.settings.volMusic *= 10; // Javascript rulez
+
+          if (key === 'right') window.settings.volMusic++;
+          else if (key === 'left') window.settings.volMusic--;
+
+          window.settings.volMusic = Phaser.Math.Clamp(window.settings.volMusic, 0, 10);
+
+          if (window.settings.volMusic == 0) this.bgm.pause()
+          else this.bgm.resume()
+
+          window.settings.volMusic /= 10;
+
+          this.bgm.setVolume(window.settings.volMusic)
         break;
-      }
-    } else if (key === 'space') {
-      switch (selected) {
-        case 2:
-          particles = !particles
-          localStorage.setItem('particles', particles)
-          if (particles) {
-            this.particlesCB.setVisible(false)
-            this.particlesCBOk.setVisible(true)
-          } else {
-            this.particlesCB.setVisible(true)
-            this.particlesCBOk.setVisible(false)
-          }
+      case 2:
+        if (key !== 'space') break;
+
+        window.settings.particles = !window.settings.particles
         break;
-      }
     }
+
+    this.refreshGui()
   }
 
   select (key) {
     switch (key) {
       case 'up':
-        if (selected > 0 && selected <= 3) {
+        if (selected > 0 && selected <= this.selectedText.length) {
           selected--;
           this.blinkSelected(selected);
         }
       break;
       case 'down':
-        if (selected >= 0 && selected < 3) {
+        if (selected >= 0 && selected < this.selectedText.length) {
           selected++;
           this.blinkSelected(selected);
         }
@@ -194,27 +147,56 @@ export default class extends Phaser.Scene {
   }
 
   blinkSelected (selected) {
-    var selectedText = [this.effectText, this.musicText, this.particlesText, this.oncheText];
+    if (this.selectedEffect !== undefined) this.selectedEffect.remove()
 
-    this.selectedEffect.remove()
-
-    for (var i = 0; i < selectedText.length; i++) { // reset alpha to 1 of all text in case tweens set alpha to 0
-      selectedText[i].setAlpha(1) 
+    for (var i = 0; i < this.selectedText.length; i++) { // reset alpha to 1 of all text in case tweens set alpha to 0
+      this.selectedText[i].setAlpha(1)
     }
 
     this.selectedEffect = this.tweens.add({
-      targets: selectedText[selected], 
+      targets: this.selectedText[selected],
       alpha: 0,
-      duration: 0, 
-      ease: 'Sine.easeInOut', 
-      repeat: -1, 
-      repeatDelay: 500, 
-      hold: 500, 
+      duration: 0,
+      ease: 'Sine.easeInOut',
+      repeat: -1,
+      repeatDelay: 500,
+      hold: 500,
       yoyo: true
     });
   }
+
+  refreshGui() {
+    this.rSelect.setVisible(true)
+    this.lSelect.setVisible(true)
+
+    if (window.settings.volEffect == 1) {
+      this.rSelect.setVisible(false)
+    } else if (window.settings.volEffect == 0) {
+      this.lSelect.setVisible(false)
+    }
+
+    this.rSelect2.setVisible(true)
+    this.lSelect2.setVisible(true)
+
+    if (window.settings.volMusic == 1) {
+      this.rSelect2.setVisible(false)
+    } else if (window.settings.volMusic == 0) {
+      this.lSelect2.setVisible(false)
+    }
+
+    this.volEffectText.setText(window.settings.volEffect * 10)
+    this.volMusicText.setText(window.settings.volMusic * 10)
+
+    this.particlesCB.setTexture(window.settings.particles ? 'checkbox' : 'checkbox_ok')
+  }
+
+  saveSettings(){
+    localStorage.setItem('settings', JSON.stringify(window.settings));
+  }
+
   goToMenu () {
     this.bgm.stop()
+    this.saveSettings()
     this.scene.start('MenuScene')
   }
 }
